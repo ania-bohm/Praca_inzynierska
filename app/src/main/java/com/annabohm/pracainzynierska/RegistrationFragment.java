d3d000febeb5b1e7ada36cf8a698e44cf6c4b54e
Ania <ania.bohm@gmail.com>
2021-03-31 17:49:46 +0200

    recyclerView shadow fixed, back from registration to login done

--- app/src/main/java/com/annabohm/pracainzynierska/RegistrationFragment.java
+++ app/src/main/java/com/annabohm/pracainzynierska/RegistrationFragment.java
@@ -34,7 +34,7 @@ public class RegistrationFragment extends Fragment {
 
     private static final String TAG = "DEV'S TAG";
     NavController navController;
-    Button createAccountButton;
+    Button createAccountButton, backToLoginButton;
     ImageView registerShowPasswordButton;
     EditText registerFirstNameEditText, registerLastNameEditText, registerEmailEditText, registerPhoneNumberEditText, registerPasswordEditText;
     FirebaseAuth firebaseAuth;
@@ -43,6 +43,58 @@ public class RegistrationFragment extends Fragment {
     ProgressBar registerProgressBar;
     Context context;
     String userID, registerFirstName, registerLastName, registerEmail, registerPassword, registerPhoneNumber;
+
+    public RegistrationFragment() {
+        // Required empty public constructor
+    }
+
+    public static RegistrationFragment newInstance(String param1, String param2) {
+        RegistrationFragment fragment = new RegistrationFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ((MainActivity) getActivity()).setDrawerLocked();
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        createAccountButton = view.findViewById(R.id.createAccountButton);
+        backToLoginButton = view.findViewById(R.id.backToLoginButton);
+        registerShowPasswordButton = view.findViewById(R.id.registerShowPasswordButton);
+        registerFirstNameEditText = view.findViewById(R.id.registerFirstNameEditText);
+        registerLastNameEditText = view.findViewById(R.id.registerLastNameEditText);
+        registerEmailEditText = view.findViewById(R.id.registerEmailEditText);
+        registerPhoneNumberEditText = view.findViewById(R.id.registerPhoneNumberEditText);
+        registerPasswordEditText = view.findViewById(R.id.registerPasswordEditText);
+        registerProgressBar = view.findViewById(R.id.registerProgressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        createAccountButton.setOnClickListener(registerOnClickListener);
+        backToLoginButton.setOnClickListener(backToLoginOnClickListener);
+    }
+
+    private View.OnClickListener backToLoginOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            navController.popBackStack();
+        }
+    };
+
     private View.OnClickListener registerOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -85,48 +137,6 @@ public class RegistrationFragment extends Fragment {
             checkEmailExistsOrNot(registerEmailEditText, registerPasswordEditText, registerFirstNameEditText, registerLastNameEditText, registerPhoneNumberEditText);
         }
     };
-
-    public RegistrationFragment() {
-        // Required empty public constructor
-    }
-
-    public static RegistrationFragment newInstance(String param1, String param2) {
-        RegistrationFragment fragment = new RegistrationFragment();
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        context = getContext();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ((MainActivity) getActivity()).setDrawerLocked();
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        navController = Navigation.findNavController(view);
-        createAccountButton = view.findViewById(R.id.createAccountButton);
-        registerShowPasswordButton = view.findViewById(R.id.registerShowPasswordButton);
-        registerFirstNameEditText = view.findViewById(R.id.registerFirstNameEditText);
-        registerLastNameEditText = view.findViewById(R.id.registerLastNameEditText);
-        registerEmailEditText = view.findViewById(R.id.registerEmailEditText);
-        registerPhoneNumberEditText = view.findViewById(R.id.registerPhoneNumberEditText);
-        registerPasswordEditText = view.findViewById(R.id.registerPasswordEditText);
-        registerProgressBar = view.findViewById(R.id.registerProgressBar);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
-
-        createAccountButton.setOnClickListener(registerOnClickListener);
-    }
 
     private void checkEmailExistsOrNot(final EditText registerEmailEditText, final EditText registerPasswordEditText, final EditText registerFirstNameEditText, final EditText registerLastNameEditText, final EditText registerPhoneNumberEditText) {
         firebaseAuth.fetchSignInMethodsForEmail(registerEmailEditText.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
