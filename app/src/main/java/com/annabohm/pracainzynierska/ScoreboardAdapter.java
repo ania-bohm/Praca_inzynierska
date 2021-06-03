13a8c5bdb3397fe45c9fca7e5a3b87749b1e07c7
Ania <ania.bohm@gmail.com>
2021-06-03 19:44:52 +0200

    scoreboard

---
+++ app/src/main/java/com/annabohm/pracainzynierska/ScoreboardAdapter.java
@@ -0,0 +1,151 @@
+package com.annabohm.pracainzynierska;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
+
+public class ScoreboardAdapter extends RecyclerView.Adapter<ScoreboardAdapter.ScoreItemHolder> {
+    ArrayList<Score> scoreList;
+    String eventId, currentUserId, eventAuthorId;
+    ScoreboardFragment scoreboardFragment;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference scoreLists = db.collection("ScoreLists");
+    CollectionReference events = db.collection("Events");
+    CollectionReference users = db.collection("Users");
+
+    public ScoreboardAdapter(ScoreboardFragment scoreboardFragment, ArrayList<Score> scoreList, String eventId, String currentUserId, String eventAuthorId) {
+        this.scoreboardFragment = scoreboardFragment;
+        this.scoreList = scoreList;
+        this.eventId = eventId;
+        this.currentUserId = currentUserId;
+        this.eventAuthorId = eventAuthorId;
+    }
+
+    @NonNull
+    @Override
+    public ScoreItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.score_list_item, parent, false);
+        return new ScoreItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ScoreItemHolder holder, final int position) {
+        holder.scoreItemGuestNameTextView.setText(scoreList.get(position).getScoreGuestName());
+        holder.scoreItemValueTextView.setText(scoreList.get(position).getScoreValue());
+
+        holder.setItemClickListener(new ScoreboardAdapter.ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                scoreboardFragment.scoreboardGuestNameMaterialEditText.setText(scoreList.get(position).getScoreGuestName());
+                scoreboardFragment.scoreboardValueMaterialEditText.setText(scoreList.get(position).getScoreValue());
+                scoreboardFragment.isUpdate = true;
+                scoreboardFragment.scoreItemToUpdateId = scoreList.get(position).getScoreId();
+            }
+        });
+
+        holder.scoreDecrementValueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int newValue = scoreList.get(position).getScoreValue() - 1;
+                scoreLists.document(eventId).collection("ScoreList").document(scoreList.get(position).getScoreId()).update("scoreValue", newValue).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        scoreList.get(position).setScoreValue(newValue);
+                        notifyDataSetChanged();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, e.toString());
+                    }
+                });
+            }
+        });
+
+        holder.scoreIncrementValueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int newValue = scoreList.get(position).getScoreValue() + 1;
+                scoreLists.document(eventId).collection("ScoreList").document(scoreList.get(position).getScoreId()).update("scoreValue", newValue).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        scoreList.get(position).setScoreValue(newValue);
+                        notifyDataSetChanged();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, e.toString());
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return scoreList.size();
+    }
+
+
+    public interface ItemClickListener {
+        void onClick(View view, int position, boolean isLongClick);
+    }
+
+
+    public static class ScoreItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener {
+        ScoreboardAdapter.ItemClickListener itemClickListener;
+        TextView scoreItemGuestNameTextView, scoreItemValueTextView;
+        FloatingActionButton scoreDecrementValueButton, scoreIncrementValueButton;
+
+        public ScoreItemHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            itemView.setOnCreateContextMenuListener(this);
+            scoreItemGuestNameTextView = itemView.findViewById(R.id.scoreItemGuestNameTextView);
+            scoreItemValueTextView = itemView.findViewById(R.id.scoreItemValueTextView);
+            scoreDecrementValueButton = itemView.findViewById(R.id.scoreDecrementValueButton);
+            scoreIncrementValueButton = itemView.findViewById(R.id.scoreIncrementValueButton);
+        }
+
+        public void setItemClickListener(ScoreboardAdapter.ItemClickListener itemClickListener) {
+            this.itemClickListener = itemClickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            itemClickListener.onClick(v, getAdapterPosition(), false);
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            menu.setHeaderTitle(R.string.score_select_action);
+            menu.add(0, 0, getAdapterPosition(), R.string.score_delete);
+            menu.add(0, 0, getAdapterPosition(), R.string.score_cancel);
+        }
+    }
+}
--- app/src/main/java/com/annabohm/pracainzynierska/ScoreboardFragment.java
+++ app/src/main/java/com/annabohm/pracainzynierska/ScoreboardFragment.java
@@ -9,31 +9,64 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import dmax.dialog.SpotsDialog;
+
 import static android.content.ContentValues.TAG;
+
 public class ScoreboardFragment extends Fragment {
+    ScoreboardFragment fragmentThis;
     NavController navController;
     AlertDialog alertDialog;
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    CollectionReference commonExpenseLists = db.collection("CommonExpenseLists");
     CollectionReference events = db.collection("Events");
     CollectionReference users = db.collection("Users");
     CollectionReference eventAttendees = db.collection("EventAttendees");
+    CollectionReference scoreLists = db.collection("ScoreLists");
+    MaterialEditText scoreboardGuestNameMaterialEditText, scoreboardValueMaterialEditText;
+    FloatingActionButton scoreFloatingActionButton;
+    RecyclerView scoreRecyclerView;
     DocumentReference eventReference;
     Context context;
     Bundle bundle;
-    String eventId, eventAuthor;
+    String eventId, eventAuthorId, currentUserId;
+    boolean isUpdate = false;
+    String scoreItemToUpdateId = "";
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<Score> scoreList;
+    ScoreboardAdapter scoreboardAdapter;
 
     public ScoreboardFragment() {
 
@@ -48,6 +81,7 @@ public class ScoreboardFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = getContext();
+        fragmentThis = this;
     }
 
     @Override
@@ -66,6 +100,18 @@ public class ScoreboardFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
 
+        currentUserId = firebaseAuth.getCurrentUser().getUid();
+
+        scoreboardGuestNameMaterialEditText = view.findViewById(R.id.scoreboardGuestNameMaterialEditText);
+        scoreboardValueMaterialEditText = view.findViewById(R.id.scoreboardValueMaterialEditText);
+        scoreFloatingActionButton = view.findViewById(R.id.scoreFloatingActionButton);
+        scoreRecyclerView = view.findViewById(R.id.scoreRecyclerView);
+
+        scoreRecyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(context);
+        scoreRecyclerView.setLayoutManager(layoutManager);
+
+        scoreList = new ArrayList<>();
         alertDialog = new SpotsDialog(context);
 
         bundle = this.getArguments();
@@ -73,6 +119,131 @@ public class ScoreboardFragment extends Fragment {
         eventReference = db.document(path);
         eventId = eventReference.getId();
 
+        eventReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                eventAuthorId = documentSnapshot.toObject(Event.class).getEventAuthor();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        });
 
+        scoreFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (scoreboardGuestNameMaterialEditText.getText().toString().trim().isEmpty() || scoreboardValueMaterialEditText.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(context, R.string.event_budget_empty_fields, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!isUpdate) {
+                    int scoreboardValue = Integer.valueOf(scoreboardValueMaterialEditText.getText().toString().trim());
+                    setData(scoreboardGuestNameMaterialEditText.getText().toString().trim(), scoreboardValue);
+                } else {
+                    int scoreboardValue = Integer.valueOf(scoreboardValueMaterialEditText.getText().toString().trim());
+                    updateData(scoreboardGuestNameMaterialEditText.getText().toString().trim(), scoreboardValue);
+                    isUpdate = !isUpdate;
+                }
+                scoreboardGuestNameMaterialEditText.setText("");
+                scoreboardValueMaterialEditText.setText("");
+            }
+        });
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if (item.getTitle().equals(getString(R.string.score_delete))) {
+            if (currentUserId.equals(eventAuthorId)) {
+                deleteItem(item.getOrder());
+            } else {
+                Toast.makeText(context, R.string.common_expense_error, Toast.LENGTH_SHORT).show();
+            }
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteItem(int index) {
+        scoreLists.document(eventId).collection("ScoreList").document(scoreList.get(index).getScoreId()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                loadData();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        });
+    }
+
+    private void updateData(String guestName, int value) {
+        scoreLists.document(eventId)
+                .collection("ScoreList")
+                .document(scoreItemToUpdateId)
+                .update("scoreGuestName", guestName, "scoreValue", value)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        });
+        scoreLists.document(eventId).collection("ScoreList").document(scoreItemToUpdateId).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                loadData();
+            }
+        });
+    }
+
+    private void setData(String guestName, int value) {
+        Map<String, Object> scoreObject = new HashMap<>();
+        String id = UUID.randomUUID().toString();
+        scoreObject.put("scoreId", id);
+        scoreObject.put("scoreGuestName", guestName);
+        scoreObject.put("scoreValue", value);
+        scoreLists.document(eventId).collection("ScoreList").document(id).set(scoreObject).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                loadData();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        });
+    }
+
+    public void loadData() {
+        if (scoreList.size() > 0) {
+            scoreList.clear();
+        }
+        alertDialog.show();
+        scoreLists.document(eventId).collection("ScoreList").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (DocumentSnapshot documentSnapshot : task.getResult()) {
+                    if (documentSnapshot.exists()) {
+                        Score score = documentSnapshot.toObject(Score.class);
+                        scoreList.add(score);
+                    }
+                }
+                scoreboardAdapter = new ScoreboardAdapter(fragmentThis, scoreList, eventId,currentUserId, eventAuthorId);
+                scoreRecyclerView.setAdapter(scoreboardAdapter);
+                alertDialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        });
     }
 }
